96be413e649135cac9de4a02486d15cb25983533
HIMEUR Bilel <bilelhimeur@gmail.com>
2019-07-22 10:35:17 +0200

    Using @Bean to create RestTemplate instance

--- movie-catalog-service/src/main/java/io/zsoftconsulting/moviecatalogservice/MovieCatalogServiceApplication.java
+++ movie-catalog-service/src/main/java/io/zsoftconsulting/moviecatalogservice/MovieCatalogServiceApplication.java
@@ -2,9 +2,17 @@ package io.zsoftconsulting.moviecatalogservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.context.annotation.Bean;
+import org.springframework.web.client.RestTemplate;
 
 @SpringBootApplication
 public class MovieCatalogServiceApplication {
+
+	// Method executed ones and produces one RestTemplate object that can be used in all project due to use of @Bean
+	@Bean
+	public RestTemplate getRestTemplate () {
+		return new RestTemplate();
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(MovieCatalogServiceApplication.class, args);
--- movie-catalog-service/src/main/java/io/zsoftconsulting/moviecatalogservice/resources/MovieCatalogResource.java
+++ movie-catalog-service/src/main/java/io/zsoftconsulting/moviecatalogservice/resources/MovieCatalogResource.java
@@ -3,13 +3,13 @@ package io.zsoftconsulting.moviecatalogservice.resources;
 import io.zsoftconsulting.moviecatalogservice.models.CatalogItem;
 import io.zsoftconsulting.moviecatalogservice.models.Movie;
 import io.zsoftconsulting.moviecatalogservice.models.Rating;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
 
 import java.util.Arrays;
-import java.util.Collections;
 import java.util.List;
 import java.util.stream.Collectors;
 
@@ -17,9 +17,12 @@ import java.util.stream.Collectors;
 @RequestMapping("/catalog")
 public class MovieCatalogResource {
 
+    // @Autowired means : search in all project for an instance of RestTemplate and give it to me
+    @Autowired
+    RestTemplate restTemplate;
+
     @RequestMapping("{userId}")
     public List<CatalogItem> getCatalog(@PathVariable("userId") String userId) {
-        RestTemplate restTemplate = new RestTemplate();
         List<Rating> ratings = Arrays.asList(
                 new Rating("1234", 4),
                 new Rating("5678", 3)
