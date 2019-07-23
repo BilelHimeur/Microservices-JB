70b4b8d012cf221978a0235f15da1f6a685963e0
HIMEUR Bilel <bilelhimeur@gmail.com>
2019-07-23 15:17:00 +0200

    Using webClient to make API calls

--- movie-catalog-service/src/main/java/io/zsoftconsulting/moviecatalogservice/resources/MovieCatalogResource.java
+++ movie-catalog-service/src/main/java/io/zsoftconsulting/moviecatalogservice/resources/MovieCatalogResource.java
@@ -8,6 +8,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.client.RestTemplate;
+import org.springframework.web.reactive.function.client.WebClient;
 
 import java.util.Arrays;
 import java.util.List;
@@ -19,7 +20,10 @@ public class MovieCatalogResource {
 
     // @Autowired means : search in all project for an instance of RestTemplate and give it to me
     @Autowired
-    RestTemplate restTemplate;
+    private RestTemplate restTemplate;
+
+    @Autowired
+    private WebClient.Builder webClientBuilder;
 
     @RequestMapping("{userId}")
     public List<CatalogItem> getCatalog(@PathVariable("userId") String userId) {
@@ -29,7 +33,15 @@ public class MovieCatalogResource {
         );
         return ratings.stream()
                 .map(rating -> {
-                    Movie movie = restTemplate.getForObject("http://localhost:8082/movies/" + rating.getMovieId(), Movie.class);
+                    //Movie movie = restTemplate.getForObject("http://localhost:8082/movies/" + rating.getMovieId(), Movie.class);
+
+                    Movie movie = webClientBuilder.build()
+                            .get()
+                            .uri("http://localhost:8082/movies/" + rating.getMovieId())
+                            .retrieve()
+                            .bodyToMono(Movie.class) // mono tells the program that you will get movie object in the future and not necessary right now (asynchronous)
+                            .block();
+
                     return new CatalogItem(movie.getName(), "des", rating.getRating());
                 }).collect(Collectors.toList());
     }
