8af6007bd7338424ecfedf20ffe7a2aac4381cd2
HIMEUR Bilel <bilelhimeur@gmail.com>
2019-08-01 12:20:24 +0200

    Adding Rating Data Servie and Movie Catalog Service as Eureka Clients

--- movie-catalog-service/src/main/java/io/zsoftconsulting/moviecatalogservice/MovieCatalogServiceApplication.java
+++ movie-catalog-service/src/main/java/io/zsoftconsulting/moviecatalogservice/MovieCatalogServiceApplication.java
@@ -2,11 +2,13 @@ package io.zsoftconsulting.moviecatalogservice;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.context.annotation.Bean;
 import org.springframework.web.client.RestTemplate;
 import org.springframework.web.reactive.function.client.WebClient;
 
 @SpringBootApplication
+@EnableEurekaClient
 public class MovieCatalogServiceApplication {
 
 	// Method executed ones and produces one RestTemplate object that can be used in all project due to use of @Bean
